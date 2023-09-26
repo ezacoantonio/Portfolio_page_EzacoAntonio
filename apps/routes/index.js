35e676f9b6ffe999716e9081c6cc9eae8eb72022
ezacoantonio <ezacoantonio@gmail.com>
2023-09-25 23:07:57 -0400

    Waiting for review and help

--- apps/routes/index.js
+++ apps/routes/index.js
@@ -28,22 +28,6 @@ router.get('/about', function(req, res, next) {
     res.render('contact');
   });
 
-
-  router.get('/Resume', function(req, res, next) {
-    res.render('Resume');
-  });
-
-  router.get('/project1', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/static-projects/project1/index.html'));
-  });
-
-  router.get('/project1', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/static-projects/project2/index.html'));
-  });
-
-  router.get('/project1', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/static-projects/project3/index.html'));
-  });
   
   router.get('/resume', function(req, res, next) {
     var filePath = path.join(__dirname, '../public/Resume Ezaco Antonio.pdf'); // Replace 'your-file-name.pdf' with your actual file name
